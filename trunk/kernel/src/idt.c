63f42130fb8267cc3b7cd46547308d260e8f6885
mecinskas <mecinskas@e09a8023-6229-0410-83fb-e179abc49278>
2007-05-30 21:15:21 +0000

    DPL3 in da house! /me dances :)-/<     :)-\-|      :)-/-\

--- trunk/kernel/src/idt.c
+++ trunk/kernel/src/idt.c
@@ -7,10 +7,12 @@
 #define NUM_ISRS            32
 
 extern dword_t gGdtCsSel;
+extern dword_t gGdtUserCsSel;
 extern void idt_load(); /* assembler fuction with 'lidt' instruction */
 
 #define KERNEL_CODE_SEGMENT gGdtCsSel
-#define IDT_FLAGS           0x8E /* entry is present, ring 0 */
+// #define IDT_FLAGS           0x8E /* entry is present, ring 0 */
+#define IDT_FLAGS           0xEE /* entry is present, ring 3 */
 
 #define PIC1                0x20
 #define PIC2                0xA0
--- trunk/kernel/src/timer.c
+++ trunk/kernel/src/timer.c
@@ -24,7 +24,7 @@ timer_schedule(void)
         // DUMP(gpActiveTaskRingNode->pTask);
         // DUMP(gpActiveTaskRingNode->pNext);
         // DUMP(gpActiveTaskRingNode->pPrev);
-        printf("Switched to task %x (parent =  %d, id = %d)\n", gpActiveTask, gpActiveTask->parent, gpActiveTask->id);
+        //printf("Switched to task %x (parent =  %d, id = %d)\n", gpActiveTask, gpActiveTask->parent, gpActiveTask->id);
     } while(gpActiveTask->locked == TRUE);
 }
 
@@ -65,22 +65,17 @@ timer_handler(regs_t * apRegs)
     vga_set_bg_color(bg);
     vga_set_fg_color(fg);
 
-    if(gpActiveTask != NULL && gpActiveTask->id != 0)
-    {
-        BRAG("!!!\n");
-    }
+    //if(gpActiveTask != NULL && gTimerTicks % 1000 == 0)
+    //{
 
-    if(gpActiveTask != NULL && gTimerTicks % 1000 == 0)
-    {
-
-        printf("Active task %x (parent =  %d, id = %d)\n", gpActiveTask, gpActiveTask->parent, gpActiveTask->id);
+        //printf("Active task %x (parent =  %d, id = %d)\n", gpActiveTask, gpActiveTask->parent, gpActiveTask->id);
         // gpActiveTaskRingNode = gpActiveTaskRingNode;
         // DUMP(gpActiveTaskRingNode);
         // DUMP(gpActiveTaskRingNode->pTask);
         // DUMP(gpActiveTaskRingNode->pNext);
         // DUMP(gpActiveTaskRingNode->pPrev);
-    }
-    if(gpActiveTask != NULL && gTimerTicks % 5000 == 0)
+    //}
+    if(gpActiveTask != NULL && gTimerTicks % 10 == 0)
     {
         timer_schedule();
     }
