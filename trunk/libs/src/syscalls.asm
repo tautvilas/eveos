66d3914a418e30fc35290546e95bd606277da2bf
d.giedrius <d.giedrius@e09a8023-6229-0410-83fb-e179abc49278>
2007-06-01 04:26:10 +0000

    exec sys call

--- trunk/libs/src/syscalls.asm
+++ trunk/libs/src/syscalls.asm
@@ -2,9 +2,11 @@ bits 32
 
 global _sys_write
 global _sys_read
+global _exec
 
 SYS         equ 69
 SYS_READ    equ 3
+SYS_EXEC    equ 11
 
 SECTION .text
 
@@ -32,6 +34,30 @@ _sys_read:
     mov     ebx, [ebp + 4]     ; file descriptor
     mov     ecx, [ebp + 8]     ; string offset
     mov     edx, [ebp + 12]     ; numbytes to read
+    int     SYS
+
+    pop     ebx
+    pop     ecx
+    pop     edx
+
+    pop     ebp
+    ; popa
+    ret
+
+_exec:
+    ; pusha
+    push    ebp
+    mov     ebp, esp
+    add     ebp, 4
+
+    push    edx
+    push    ecx
+    push    ebx
+
+    mov     eax, SYS_EXEC       ; syscall id
+    mov     ecx, [ebp + 4]     ; string offset
+    mov     edx, [ebp + 8]      ; priority
+    mov     ebx, [ebp + 12]      ; async?
     int     SYS
 
     pop     ebx
--- trunk/libs/src/syscalls.h
+++ trunk/libs/src/syscalls.h
@@ -1,7 +1,15 @@
 #ifndef _SYSCALLS_H_
 #define _SYSCALLS_H_
 
+
+typedef enum {
+    PRIOR_LOW = 0,
+    PRIOR_NORMAL,
+    PRIOR_HIGH
+} priority_t;
+
 extern void __attribute__((cdecl)) sys_write(const char* apStr, int aLen);
 extern int __attribute__((cdecl)) sys_read(int aFd, char* apBuffer, int aLen);
+extern int __attribute__((cdecl)) exec(const char* apFileName, priority_t aPriority, int aAsync);
 
 #endif // _SYSCALLS_H_
