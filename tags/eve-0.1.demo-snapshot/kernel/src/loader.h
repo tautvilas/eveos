0be1cbbc898d59e53c9a0c32eeff8ef538acd920
mecinskas <mecinskas@e09a8023-6229-0410-83fb-e179abc49278>
2007-06-05 00:28:24 +0000

    Copied remotely

---
+++ tags/eve-0.1.demo-snapshot/kernel/src/loader.h
@@ -0,0 +1,68 @@
+#ifndef _LOADER_H_
+#define _LOADER_H_
+
+#include "global.h"
+#include "memmgr.h"
+
+/* WARNING!!! if YOU change this struct you also mus change common interrupt handlers */
+typedef struct {
+    dword_t         esp;    //actual position of esp
+    size_t          id;
+    dword_t         page_dir;
+    dword_t         ss;     //actual stack segment.
+    dword_t         kstack; //stacktop of kernel stack
+    dword_t         ustack; //stacktop of user stack
+    dword_t         locked;
+    size_t          parent;
+    size_t          timetorun;
+    size_t          sleep;
+    size_t          priority;
+    mm_access_t     access;
+    mm_task_mem_t   vm_info;
+} task_t;
+
+typedef enum {
+    PRIOR_LOW = 0,
+    PRIOR_NORMAL,
+    PRIOR_HIGH
+} priority_t;
+
+typedef struct task_tree_node_t {
+    task_t*                     pTask;
+    struct task_tree_node_t*    pNext;
+    struct task_tree_node_t*    pPrev;
+    struct task_tree_node_t*    pParent;
+    struct task_tree_node_t*    pFirstChild;
+} task_tree_node_t;
+
+typedef struct task_ring_node_t {
+    task_t*                     pTask;
+    struct task_ring_node_t*    pNext;
+    struct task_ring_node_t*    pPrev;
+    struct task_tree_node_t*    pTreeNode;
+} task_ring_node_t;
+
+task_ring_node_t* KERNEL_CALL
+multitasking_install(void);
+
+void KERNEL_CALL
+print_task_tree(void);
+
+int KERNEL_CALL
+kill_task(uint_t id);
+
+extern dword_t gPingTaskOffset;
+extern dword_t gEshTaskOffset;
+extern dword_t gKernelTaskOffset;
+
+task_ring_node_t* KERNEL_CALL
+load_task(void* apOffset, task_ring_node_t* apParent, mm_access_t aAccess, priority_t aPrior, bool_t aOnTop);
+
+extern task_t* gpActiveTask;
+extern task_t* gpForegroundTask;
+extern uint_t gpPriorityTimes[];
+extern task_ring_node_t* gpActiveTaskRingNode;
+extern dword_t gNextTaskOffset;
+extern dword_t gKernelCr3;
+
+#endif // _LOADER_H_
---
+++ tags/eve-0.1.demo-snapshot/kernel/src/mallocdefs.h
@@ -0,0 +1,27 @@
+#ifndef _MALLOCDEFS_H_
+#define _MALLOCDEFS_H_
+
+#include "memmgr.h"
+#include "global.h"
+#include "idt.h"
+#include "mem.h"
+#include "errno.h"
+
+
+#define ABORT               kernel_panic();
+#define HAVE_MMAP           0
+#define HAVE_MREMAP         0
+#define MMAP_CLEARS         0
+#define malloc_getpagesize  MM_PAGE_SIZE
+#define LACKS_UNISTD_H
+#define LACKS_FCNTL_H
+#define LACKS_SYS_PARAM_H
+#define LACKS_SYS_MMAN_H
+#define LACKS_STRINGS_H
+#define LACKS_STRING_H
+#define LACKS_SYS_TYPES_H
+#define LACKS_ERRNO_H
+#define LACKS_STDLIB_H
+#define DEFAULT_TRIM_THRESHOLD  MEGABYTE
+
+#endif  // _MALLOCDEFS_H_
